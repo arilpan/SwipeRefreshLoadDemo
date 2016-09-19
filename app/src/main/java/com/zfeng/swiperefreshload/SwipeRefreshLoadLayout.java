f44d74d5a40108890f86ae83e0aac4567c81ab13
arilpan <1045375318@qq.com>
2016-09-19 10:06:59 +0800

    add multi layout manager support.

--- app/src/main/java/com/zfeng/swiperefreshload/SwipeRefreshLoadLayout.java
+++ app/src/main/java/com/zfeng/swiperefreshload/SwipeRefreshLoadLayout.java
@@ -6,8 +6,10 @@ import android.content.res.TypedArray;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -21,6 +23,7 @@ import android.view.animation.Transformation;
 import android.widget.AbsListView;
 
 /**
+ * Modify by arilpan on 2016-9-13
  * Created by zfeng on 2015-2-26.
  */
 public class SwipeRefreshLoadLayout extends ViewGroup {
@@ -650,9 +653,25 @@ public class SwipeRefreshLoadLayout extends ViewGroup {
                     }else if(mTarget instanceof RecyclerView)
                     {
                         final RecyclerView recyclerView=(RecyclerView)mTarget;
-                        LinearLayoutManager lLManager=(LinearLayoutManager)recyclerView.getLayoutManager();
-                        if((lLManager.findLastVisibleItemPosition()+1)==lLManager.getItemCount())
-                        {
+                        //修改：适应多种 LayoutManager
+                        int lastPosition = -1;
+                        RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+                        if(layoutManager instanceof GridLayoutManager){
+                            //通过LayoutManager找到当前显示的最后的item的position
+                            lastPosition = ((GridLayoutManager) layoutManager).findLastVisibleItemPosition();
+                        }else if(layoutManager instanceof LinearLayoutManager){
+                            lastPosition = ((LinearLayoutManager) layoutManager).findLastVisibleItemPosition();
+                        }else if(layoutManager instanceof StaggeredGridLayoutManager){
+                            //因为StaggeredGridLayoutManager的特殊性可能导致最后显示的item存在多个，所以这里取到的是一个数组
+                            //得到这个数组后再取到数组中position值最大的那个就是最后显示的position值了
+                            int[] lastPositions = new int[((StaggeredGridLayoutManager) layoutManager).getSpanCount()];
+                            ((StaggeredGridLayoutManager) layoutManager).findLastVisibleItemPositions(lastPositions);
+                            lastPosition = findMax(lastPositions);
+                        }
+                        //时判断界面显示的最后item的position是否等于itemCount总数-1也就是最后一个item的position
+                        //如果相等则说明已经滑动到最后了
+                        if(lastPosition == recyclerView.getLayoutManager().getItemCount()-1){
+//                            Toast.makeText(RecyclerActivity.this, "滑动到底了", Toast.LENGTH_SHORT).show();
                             if(mCanLoadMore)
                             {
                                 mCanLoadMore=false;
@@ -669,7 +688,15 @@ public class SwipeRefreshLoadLayout extends ViewGroup {
 
         return mIsBeingDragged;
     }
-
+    private int findMax(int[] lastPositions) {
+        int max = lastPositions[0];
+        for (int value : lastPositions) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
+    }
     private float getMotionEventY(MotionEvent ev, int activePointerId) {
         final int index = MotionEventCompat.findPointerIndex(ev, activePointerId);
         if (index < 0) {
